73ab04561acc7fe63cfd25779875edfe0ddf4a89
bitbluedev <bitbluedev@gmail.com>
2018-11-07 23:59:03 +0100

    Implementation & readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DisableScreensaverInCs
-Disable Screensaver
+
+There can be times when you have a screensaver/screen lock after n minutes, and you don't want to, or can not disable it.
+If you can however run csc and a compiled binary there is a solution.
+Compile and run this code to prevent screen lock on a local machine, or remote desktop connection.
+
---
+++ SchreechSaver.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace HelloWorld
+{
+    //c:\Windows\Microsoft.NET\Framework\v2.0.50727\csc.exe SchreechSaver.cs
+    //c:\Windows\Microsoft.NET\Framework\v3.5\csc.exe SchreechSaver.cs
+    //c:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe SchreechSaver.cs
+    class SchreechSaver
+    {
+        private const uint ES_CONTINUOUS = 0x80000000;
+        private const uint ES_DISPLAY_REQUIRED = 0x00000002;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern uint SetThreadExecutionState([In] uint esFlags);
+
+        static void Main()
+        {
+            Console.WriteLine("F?ing up screen lock");
+
+            SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
